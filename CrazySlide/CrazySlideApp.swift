cc50250bdc0525ad7447c89f49c59dbce82fcc90
aleksey <aleksey@tetavi.com>
2021-07-09 19:47:58 +0300

    added project meat

---
+++ CrazySlide/CrazySlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrazySlideApp.swift
+//  CrazySlide
+//
+//  Created by aleksey timoshchenko on 09/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CrazySlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
